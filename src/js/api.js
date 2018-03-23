595c3cc951b3d45bdbaec25fddf2bbae56f28488
Ian Goodrich and Jasun Feddema <jasun@>
2018-03-22 19:03:16 -0700

    add parseResponce function to clear out front end. Add functionality to clear #results div on each new search. Add exception handling for null result

--- src/js/api.js
+++ src/js/api.js
@@ -1,13 +1,13 @@
 import $ from 'jquery';
 
-export class AsteroidApi{
+export class AsteroidApi {
 
-  getRoids(startD, endD){
+  getRoids(startD, endD) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest()
       let url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startD}&end_date=${endD}&api_key=xTCFCcr8WZts1nzv144pnN4yxxukx4GGDRoOhesX`
-      request.onload = function(){
-        if (this.status == 200){
+      request.onload = function() {
+        if (this.status == 200) {
           resolve(request.response)
 
         } else {
@@ -18,4 +18,34 @@ export class AsteroidApi{
       request.send()
     })
   }
+
+  parseResponce(response) {
+    let count = 0;
+    let asteroid = JSON.parse(response)
+    // console.log(asteroid.near_earth_objects);  SAVE   THIS IS USEFUL TO LEAN HAOW TO PARSE THE API DATA
+    for (let i = 0; i < Object.keys(asteroid.near_earth_objects).length; i++) {
+      let thisAsteroid = Object.keys(asteroid.near_earth_objects)[i];
+      for (let j = 0; j < asteroid.near_earth_objects[thisAsteroid].length; j++) {
+        let threat = asteroid.near_earth_objects[thisAsteroid][j].is_potentially_hazardous_asteroid
+        if (threat == true) {
+          count += 1
+          let distance = asteroid.near_earth_objects[thisAsteroid][j].close_approach_data[0].miss_distance.miles;
+          let velocity = asteroid.near_earth_objects[thisAsteroid][j].close_approach_data[0].relative_velocity.miles_per_hour;
+          let velocityF = Math.floor(velocity)
+          let timeUntilImpact = Math.floor((distance / velocity) / 24);
+          $("#results").append(
+            "<div class='w3-container w3-row'>" +
+            "<div class='w3-quarter'> " + `${asteroid.near_earth_objects[thisAsteroid][j].name}` + "</div>" +
+            "<div class='w3-quarter'> " + `${asteroid.near_earth_objects[thisAsteroid][j].close_approach_data[0].miss_distance.miles}` + "miles </div>" +
+            "<div class='w3-quarter'> " + `${velocityF}` + "mph </div>" +
+            "<div class='w3-quarter'> " + timeUntilImpact + " days </div>" +
+            "</div>" + "<br>");
+        }
+      }
+    }
+    if (count == 0) {
+      $("#results").text("There are no potentially hazardous asteroids approaching in that date range. Guess you'll have to go to work")
+    }
+  }
+
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { AsteroidApi } from "./js/api.js"
 import { solar } from "./js/solarsystem.js"
-import { exampleStuff } from "./example.js"
 import * as THREE from 'three';
 import $ from 'jquery';
 import './styles.css';
@@ -9,38 +8,19 @@ import 'three-examples/controls/OrbitControls';
 
 $(document).ready(function() {
   solar()
-  $("#roid-butt").click(function(){
-  let startD = $("#start_date").val()
-  let endD = $("#end_date").val()
-  $("#start_date").val("");
-  $("#end_date").val("");
+  $("#roid-butt").click(function() {
+    let startD = $("#start_date").val()
+    let endD = $("#end_date").val()
+    $("#start_date").val("");
+    $("#end_date").val("");
+    $("#results").html("");
 
-  let newAsteroid = new AsteroidApi;
-  let promise = newAsteroid.getRoids(startD, endD);
-  promise.then(function(response) {
-    let asteroid = JSON.parse(response)
-    console.log(asteroid.near_earth_objects);
-      for(let i = 0; i < Object.keys(asteroid.near_earth_objects).length; i++){
-      let thisAsteroid = Object.keys(asteroid.near_earth_objects)[i];
-        for(let j = 0; j < asteroid.near_earth_objects[thisAsteroid].length; j++){
-        let threat = asteroid.near_earth_objects[thisAsteroid][j].is_potentially_hazardous_asteroid
-        if (threat == true){
-          let distance = asteroid.near_earth_objects[thisAsteroid][j].close_approach_data[0].miss_distance.miles;
-          let velocity = asteroid.near_earth_objects[thisAsteroid][j].close_approach_data[0].relative_velocity.miles_per_hour;
-          let velocityF = Math.floor(velocity)
-          let timeUntilImpact = Math.floor((distance/velocity)/24);
-          $("#results").append(
-            "<div class='w3-container w3-row'>" +
-              "<div class='w3-quarter'> " + `${asteroid.near_earth_objects[thisAsteroid][j].name}` + "</div>" +
-              "<div class='w3-quarter'> " + `${asteroid.near_earth_objects[thisAsteroid][j].close_approach_data[0].miss_distance.miles}` + "miles </div>" +
-              "<div class='w3-quarter'> " + `${velocityF}` + "mph </div>" +
-              "<div class='w3-quarter'> " + timeUntilImpact + " days </div>" +
-            "</div>" + "<br>");
-        }
-      }
-    }
-}, function(error){
-  $("#results").text(`There was an error with your request : ${error.message}`)
-})
-})
+    let newAsteroid = new AsteroidApi;
+    let promise = newAsteroid.getRoids(startD, endD);
+    promise.then(function(response) {
+      newAsteroid.parseResponce(response)
+    }, function(error) {
+      $("#results").text(`There was an error with your request : ${error.message}`)
+    })
+  })
 })
